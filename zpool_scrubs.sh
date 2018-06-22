d3d1c83d40e47747f5044e1593308322f795c7d5
rjsears <richard@sears.net>
2018-06-21 18:27:02 -0700

    Create zpool_scrubs.sh

---
+++ zpool_scrubs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+# Quick script to run via cron to run system scrubs on ZFS filesystems.
+
+# Script expects to be able to record its progress in a /root/logs directory.
+
+export PATH=/usr/sbin:/usr/bin:/bin:/sbin
+LOG=/var/log/scrubs/`date +20\%y\%m\%d\%H\%M`-scrubs.log
+zpool list -H -o name | while read pool; do
+
+# If a scrub is already running, then don't try and start a second one (because you can't!)
+ if zpool status -v $pool | grep -q "scrub in progress"; then
+ echo "Can't scrub pool "$pool" -scrub already running!" >> $LOG 2>&1
+ exit
+ else
+ echo "Scrubbing pool "$pool >> $LOG 2>&1
+ zpool scrub $pool
+ fi
+
+done
+echo "Script took $SECONDS seconds to complete (scrubs run for hours in background!)." >> $LOG 2>&1
+exit 0
